123e74f8c665477008e0109f5bc674d38c05d4bf
yuuta1219 <yuuta.sakiyama.1219@gmail.com>
2023-03-20 16:01:01 +0900

    feat:categoryコントローラー、モデルの 作成。#9

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_20_065019) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_20_065956) do
   create_table "boards", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "title", null: false
@@ -18,6 +18,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_20_065019) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_boards_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
