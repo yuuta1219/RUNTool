6ff5737a25a941611f191f5ef2ab7ed50dcac502
yuuta1219 <yuuta.sakiyama.1219@gmail.com>
2023-03-23 16:15:00 +0900

    docs:メッセージの文章変更#15

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -4,15 +4,15 @@ class UserSessionsController < ApplicationController
   def create
     @user = login(params[:email], params[:password])
     if @user
-      redirect_back_or_to root_path, notice: "ログインしました。"
+      redirect_back_or_to root_path, notice: "ログインしました"
     else
-      flash.now[:alert] = "ログインできませんでした。"
+      flash.now[:alert] = "ログインできませんでした"
       render :new
     end
   end
   
   def destroy
     logout
-    redirect_to root_path, status: :see_other, notice: "ログアウトしました。"
+    redirect_to root_path, status: :see_other, notice: "ログアウトしました"
   end
 end
