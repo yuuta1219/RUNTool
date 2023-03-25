739dcae2ad9f66f99eedf6711868e2877ab546fa
yuuta1219 <yuuta.sakiyama.1219@gmail.com>
2023-03-25 17:02:50 +0900

    rails generate migration AddCategoryIdToBoards category_id:integer

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,6 +1,7 @@
 class Board < ApplicationRecord
 
   belongs_to :user
+  belongs_to :category
 
   validates :title, presence: true, length: { maximum: 255 }
   validates :body, presence: true, length: { maximum: 65_535 }
