0f287bc04dedc523a1f047729f177933ebebd426
hagoromo2000 <revtm000126@gmail.com>
2023-03-28 10:48:24 +0900

    feat:ブックマーク機能の作成 #24

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,8 +1,10 @@
 class Board < ApplicationRecord
 
+  has_many :bookmarks, dependent: :destroy
   belongs_to :user
   belongs_to :category
   belongs_to :progress
+
 
   validates :title, presence: true, length: { maximum: 255 }
   validates :body, presence: true, length: { maximum: 65_535 }
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,2 +1,6 @@
 class Bookmark < ApplicationRecord
+  belongs_to :user
+  belongs_to :board
+
+  validates :user_id, uniqueness: {scope: :board_id}
 end
